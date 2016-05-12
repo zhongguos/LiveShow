d0128d4f9c7bb69cf76ec8e59ce1b28757543b3d
lihanghang <493337309@qq.com>
2016-05-12 10:05:25 +0800

    liveshow

---
+++ app/src/main/java/ifire/liveshow/TestActivity.java
@@ -0,0 +1,18 @@
+package ifire.liveshow;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Administrator on 2016/5/10.
+ */
+public class TestActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+
+    }
+}
